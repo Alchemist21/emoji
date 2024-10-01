f3a7c1f966b5bf84e14ae56c4fe8a6d4fb26fec5
Louell Sala <louell_sala@yahoo.com>
2024-10-01 14:03:49 -0400

    Initial Commit

---
+++ emoji/emojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojiApp.swift
+//  emoji
+//
+//  Created by Louell Sala on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct emojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ emojiTests/emojiTests.swift
@@ -0,0 +1,17 @@
+//
+//  emojiTests.swift
+//  emojiTests
+//
+//  Created by Louell Sala on 10/1/24.
+//
+
+import Testing
+@testable import emoji
+
+struct emojiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
